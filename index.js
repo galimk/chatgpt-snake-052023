0559836de2920693f962baea075b13c688892a6d
galimk <galimk@gmail.com>
2023-05-06 09:55:21 -0600

    step 2.a. 2.b.

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
 // Global variables and constants
+const gridSize = 20;
+const gridDimensions = { rows: 20, columns: 20 };
+const cellSize = gridSize / gridDimensions.rows;
+const initialSnakePosition = { x: Math.floor(gridDimensions.columns / 2), y: Math.floor(gridDimensions.rows / 2) };
+const initialSnakeDirection = { x: 0, y: -1 };
+const initialSnakeSpeed = 100; // In milliseconds
+const initialCatchablePointPosition = { x: Math.floor(Math.random() * gridDimensions.columns), y: Math.floor(Math.random() * gridDimensions.rows) };
+
+let snake = [];
+let snakeDirection = { ...initialSnakeDirection };
+let snakeSpeed = initialSnakeSpeed;
+let catchablePoint = { ...initialCatchablePointPosition };
+let isGameRunning = false;
+let score = 0;
+
+const gridElement = document.getElementById("grid");
+const scoreElement = document.getElementById("score");
+
 
 // Functions
 function initializeGame() { /* ... */ }
