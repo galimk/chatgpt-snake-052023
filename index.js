d8485a11709436919001039b868282f496c39152
galimk <galimk@gmail.com>
2023-05-06 09:53:28 -0600

    step 1.a., 1.b., 1.c.

---
+++ index.js
@@ -0,0 +1,13 @@
+// Global variables and constants
+
+// Functions
+function initializeGame() { /* ... */ }
+function startGame() { /* ... */ }
+function endGame() { /* ... */ }
+function handleKeyboardInput(e) { /* ... */ }
+function gameLoop() { /* ... */ }
+
+// Event listeners
+document.getElementById('start-game').addEventListener('click', startGame);
+document.getElementById('end-game').addEventListener('click', endGame);
+document.addEventListener('keydown', handleKeyboardInput);
